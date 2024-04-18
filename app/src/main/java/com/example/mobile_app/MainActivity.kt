f27493bdabb63a5f75d190394579d0d311d196aa
Alekseev <igor.alekseev.20024@gmail.com>
2024-04-18 23:44:04 +0300

    add module of igor

--- app/src/main/java/com/example/mobile_app/MainActivity.kt
+++ app/src/main/java/com/example/mobile_app/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.mobile_app
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
@@ -22,21 +27,40 @@ import androidx.navigation.compose.rememberNavController
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.TextField
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.mobile_app.ui.theme.ThemeBlue
+import com.example.mobile_app.R
 
 
 class MainActivity : ComponentActivity() {
@@ -56,7 +80,7 @@ fun MyApp() {
             composable("login") { LoginScreen(navController) }
             composable("main") { MainScreen(navController) }
             composable("attestation") { AttestationScreen(navController) }
-            composable("shift") { ShiftScreen() }
+            composable("active_shift") { ShiftScreen(navController) }
         }
     }
 }
@@ -69,45 +93,30 @@ fun LoginScreen(navController: NavController) {
 
     Column(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        val textStyle = TextStyle(
-            fontSize = 16.sp,
-            color = Color.Black
-        )
+        Spacer(modifier = Modifier.height(24.dp))
 
-        OutlinedTextField(
-            value = username,
-            onValueChange = { username = it },
-            label = { Text("Логин", style = textStyle) },
-            modifier = Modifier
-                .padding(horizontal = 20.dp, vertical = 8.dp)
-                .fillMaxWidth(),
-            singleLine = true,
-            textStyle = textStyle,
-            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
-            keyboardActions = KeyboardActions(
-                onNext = {}
-            )
-        )
+        Image(painter = painterResource(id = R.drawable.secure_icon), contentDescription = "Login Image",
+            modifier = Modifier.size(200.dp))
 
-        OutlinedTextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Пароль", style = textStyle) },
-            modifier = Modifier
-                .padding(horizontal = 20.dp, vertical = 8.dp)
-                .fillMaxWidth(),
-            singleLine = true,
-            textStyle = textStyle,
-            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-            keyboardActions = KeyboardActions(
-                onNext = {}
-            )
-        )
+        Text(text = "Добро пожаловать!", fontSize = 28.sp, fontWeight = FontWeight.Bold)
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(12.dp))
+
+        OutlinedTextField(value = username, onValueChange = {username = it}, label = {
+            Text(text = "Логин")
+        })
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        OutlinedTextField(value = password, onValueChange = {password = it}, label = {
+            Text(text = "Пароль")
+        }, visualTransformation = PasswordVisualTransformation())
+
+        Spacer(modifier = Modifier.height(18.dp))
+
 
         Button(
             onClick = {
@@ -117,12 +126,12 @@ fun LoginScreen(navController: NavController) {
                     showErrorDialog = true
                 }
             },
-            modifier = Modifier
-                .padding(20.dp)
-                .height(48.dp)
-                .fillMaxWidth()
+            modifier = Modifier.height(50.dp).width(120.dp),
+            colors = ButtonDefaults.buttonColors(
+                contentColor = Color.White,       // цвет текста
+                containerColor = Color(0xFF0357A6))     // цвет фона
         ) {
-            Text("Войти", style = TextStyle(fontSize = 18.sp))
+            Text(text = "Войти", fontSize = 18.sp)
         }
     }
 
@@ -146,30 +155,7 @@ fun LoginScreen(navController: NavController) {
 
 @Composable
 fun MainScreen(navController: NavController) {
-    Button(
-        onClick = { navController.popBackStack() },
-        modifier = Modifier
-            .padding(8.dp),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color.Red,
-            contentColor = Color.White
-        ),
-        shape = MaterialTheme.shapes.medium,
-        elevation = ButtonDefaults.buttonElevation(
-            defaultElevation = 4.dp,
-            pressedElevation = 8.dp
-        ),
-        contentPadding = PaddingValues(16.dp),
-    ) {
-        Text(
-            "Выйти",
-            style = TextStyle(
-                fontSize = 16.sp,
-                color = Color.White,
-                fontFamily = FontFamily.Monospace
-            )
-        )
-    }
+    Header(navController)
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
@@ -189,7 +175,7 @@ fun MainScreen(navController: NavController) {
                     .padding(8.dp)
                     .fillMaxWidth(),
                 colors = ButtonDefaults.buttonColors(
-                    containerColor = Color(0, 128, 128),
+                    containerColor = Color(0xFF0357A6),
                     contentColor = Color.White
                 ),
                 shape = MaterialTheme.shapes.medium,
@@ -211,12 +197,12 @@ fun MainScreen(navController: NavController) {
             Spacer(modifier = Modifier.height(16.dp))
 
             Button(
-                onClick = { navController.navigate("shift") },
+                onClick = { navController.navigate("active_shift") },
                 modifier = Modifier
                     .padding(8.dp)
                     .fillMaxWidth(),
                 colors = ButtonDefaults.buttonColors(
-                    containerColor = Color(0, 128, 128),
+                    containerColor = Color(0xFF0357A6),
                     contentColor = Color.White
                 ),
                 shape = MaterialTheme.shapes.medium,
@@ -249,14 +235,140 @@ fun AttestationScreen(navController: NavController) {
 
     AttestationsScreen(navController = navController, attestations = attestations)
 }
+
+
+
+
+
+
+
 @Composable
-fun ShiftScreen() {
-    // экран Смена
+fun Header(navController: NavController){
+    Box(modifier = Modifier.fillMaxWidth().background(ThemeBlue)) {
+        Row (modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp)
+            .height(55.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            //            elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+        ){
+            Icon(
+                imageVector = Icons.Filled.Menu,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(40.dp)
+                    .clickable { navController.navigate("main") }
+            )
+            Text(text = "Security", color = Color.White, fontSize = 24.sp)
+            Icon(
+                imageVector = Icons.Filled.ExitToApp,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(45.dp)
+                    .padding(5.dp)
+                    .clickable { navController.navigate("login") },
+            )
+        }
+    }
+}
+
+@Composable
+fun ShiftScreen(navController: NavController) {
+    val isWorking = false
+    Column {
+        Header(navController)
+
+        Card(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp),
+            shape = RoundedCornerShape(15.dp),
+            elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+        ){
+            Column (modifier = Modifier
+                .padding(15.dp)) {
+                Row(modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween) {
+                    Text(text = "Объект: ", fontSize = 24.sp)
+                    Text(text = "ТЦ Европа", fontSize = 24.sp)
+                }
+                Row(modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween) {
+                    Text(text = "Адрес: ", fontSize = 24.sp)
+                    Text(text = "Ленина, 7", fontSize = 24.sp)
+                }
+                Spacer(modifier = Modifier.height(20.dp))
+                Row(modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween) {
+                    Text(text = "Дата: ", fontSize = 24.sp)
+                    Text(text = "11.04.2024", fontSize = 24.sp)
+                }
+                Spacer(modifier = Modifier.height(20.dp))
+                Row(modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween) {
+                    Text(text = "Начало смены: ", fontSize = 24.sp)
+                    Text(text = "09:00", fontSize = 24.sp)
+                }
+                Row(modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween) {
+                    Text(text = "Конец смены: ", fontSize = 24.sp)
+                    Text(text = "20:00", fontSize = 24.sp)
+                }
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Column(modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    val checked = rememberSaveable { mutableStateOf(isWorking) }
+                    MyButton(checked)
+
+                    Spacer(modifier = Modifier.height(20.dp))
+
+                    Button(onClick = { navController.navigate("main") },
+                        modifier = Modifier
+                            .height(50.dp)
+                            .width(120.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            contentColor = Color.White,       // цвет текста
+                            containerColor = Color(0xFF0357A6))     // цвет фона
+                    ) {
+                        Text(text = "Назад", fontSize = 18.sp)
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun MyButton(checked: MutableState<Boolean>){
+    val label = remember{mutableStateOf("Начать\nсмену")}
+
+    Box(modifier = Modifier
+        .padding(10.dp)
+        .size(180.dp)
+        .clip(CircleShape)
+        .background(
+            if (checked.value) Color(0xFFBA0000)
+            else Color(0xff007f00)
+        )
+        .toggleable(value = checked.value, onValueChange = { checked.value = it }),
+        contentAlignment = Alignment.Center
+    ){
+        label.value = if (checked.value) "Завершить\nсмену"
+        else "Начать\nсмену"
+        Text(label.value, fontSize = 25.sp, textAlign = TextAlign.Center, color = Color.White)
+    }
 }
 
 @Composable
 fun AttestationsScreen(navController: NavController, attestations: List<Attestation>) {
     Column(modifier = Modifier.fillMaxSize()) {
+        Header(navController)
+        Spacer(modifier = Modifier.height(8.dp))
         LazyColumn(modifier = Modifier.weight(1f)) {
             items(attestations) { attestation ->
                 AttestationCard(attestation = attestation)
@@ -270,7 +382,11 @@ fun AttestationsScreen(navController: NavController, attestations: List<Attestat
             },
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
-                .padding(16.dp)
+                .padding(16.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF0357A6),
+                contentColor = Color.White
+            )
         ) {
             Text("Назад")
         }
